60b3468754f560fcf1e1fb2fabc0b538d57f87bf
Armando Perez <armandlord98@gmail.com>
2023-03-07 22:19:04 -0600

    agrega types al reducer

--- src/00-context/context/character/CharacterReducer.tsx
+++ src/00-context/context/character/CharacterReducer.tsx
@@ -4,16 +4,14 @@ const CharacterReducer = (type: string, payload: [] | {}) => {
   switch (type) {
     case GET_CHARACTERS:
       return {
-        ...state,
         characters: payload,
       };
     case GET_CHARACTER:
       return {
-        ...state,
         selectedCharacter: payload,
       };
     default:
-      return state;
+      return payload;
   }
 };
 
