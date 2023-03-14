65702c6102b296370f68a47da4f7e1cd4b962911
Armando Perez <armandlord98@gmail.com>
2023-03-14 16:53:54 -0600

    limpia ShoppingPage

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -2,64 +2,23 @@ import ProductCart from "../components/ProductCart";
 import "../styles/custom-styles.css";
 import { Product } from "../interfaces/interfaces";
 import { products } from "../data/products";
-import { useShoppingCart } from "../hooks/useChoppingCart";
 
-export interface ProductInCart extends Product {
-  count: number;
-}
-
+const product = products[0];
 const ShoppingPage = () => {
-  const { cartMap, shoppingCart, onProductCartChange } = useShoppingCart();
-
   return (
     <div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-        }}
-      >
-        {products.map((product) => (
-          <ProductCart
-            key={product.id}
-            onChange={onProductCartChange}
-            className="bg-dark"
-            product={product}
-            value={shoppingCart[product.id]?.count || 0}
-          >
-            <ProductCart.Image
-              customStyles={{
-                borderRadius: "50%",
-              }}
-            />
-            <ProductCart.Title className="text-white" />
-            <ProductCart.Buttons />
-          </ProductCart>
-        ))}
-      </div>
-      <div className="shopping-cart">
-        {cartMap?.map((product) => (
-          <ProductCart
-            key={product.id}
-            className="bg-dark"
-            product={product}
-            onChange={onProductCartChange}
-            // con la propiedad value podemos controlar el estado del componente
-            value={product.count}
+      <h1>Product:</h1>
+      <hr />
+      <div>
+        <ProductCart key={product.id} className="bg-dark" product={product}>
+          <ProductCart.Image
             customStyles={{
-              width: "100px",
+              borderRadius: "50%",
             }}
-          >
-            <ProductCart.Image
-              customStyles={{
-                borderRadius: "50%",
-              }}
-            />
-            {/* <ProductCart.Title className="text-white" /> */}
-            <ProductCart.Buttons />
-          </ProductCart>
-        ))}
+          />
+          <ProductCart.Title className="text-white" />
+          <ProductCart.Buttons />
+        </ProductCart>
       </div>
     </div>
   );
