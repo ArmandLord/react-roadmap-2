9d158f75963928c95df6916fe1f92948c4378154
Armando Perez <armandlord98@gmail.com>
2023-03-19 19:38:36 -0600

    install formik

--- src/03-forms/pages/FormikPage.tsx
+++ src/03-forms/pages/FormikPage.tsx
@@ -1,4 +1,6 @@
+import { useFormik } from "formik";
 import "../styles/styles.css";
+
 const FormikPage = () => {
   return (
     <div>
@@ -6,6 +8,17 @@ const FormikPage = () => {
       <form>
         <label htmlFor="firstName">First Name</label>
         <input type="text" name="firstName" />
+        <span>Proporciona un nombre valido</span>
+        <br />
+        <label htmlFor="lastName">Last Name</label>
+        <input type="text" name="lastName" />
+        <span>Proporciona un apellido valido</span>
+        <br />
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" />
+        <span>Proporciona un email valido</span>
+        <br />
+        <button type="submit">submit</button>
       </form>
     </div>
   );
