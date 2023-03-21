a5d8c115626019ab802c3ea09b26658d56eeb513
Armando Perez <armandlord98@gmail.com>
2023-03-20 20:27:40 -0600

    creando validaciones con formik

--- src/03-forms/pages/FormikPage.tsx
+++ src/03-forms/pages/FormikPage.tsx
@@ -1,21 +1,79 @@
-import { useFormik } from "formik";
+import { FormikErrors, useFormik } from "formik";
 import "../styles/styles.css";
 
+export interface ValuesFormik {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
 const FormikPage = () => {
+  const validate = ({ firstName, lastName, email }: ValuesFormik) => {
+    const errors: FormikErrors<ValuesFormik> = {};
+
+    if (!firstName) {
+      errors.firstName = "FirstName is required";
+    } else if (firstName.length > 15) {
+      errors.firstName = "FirstName must be 15 characters or less";
+    }
+
+    if (!lastName) {
+      errors.firstName = "FirstName is required";
+    } else if (firstName.length > 10) {
+      errors.firstName = "FirstName must be 15 characters or less";
+    }
+
+    if (!email) {
+      errors.email = "Required";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+      errors.email = "Invalid email address";
+    }
+
+    return errors;
+  };
+
+  const { handleChange, handleSubmit, values } = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    // solo se dispara cuando las reglas de validacion son disparadas
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validate,
+  });
+
   return (
     <div>
       <h1>FormikPage</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="firstName">First Name</label>
-        <input type="text" name="firstName" />
+        <input
+          type="text"
+          name="firstName"
+          onChange={handleChange}
+          value={values.firstName}
+        />
         <span>Proporciona un nombre valido</span>
         <br />
         <label htmlFor="lastName">Last Name</label>
-        <input type="text" name="lastName" />
+        <input
+          type="text"
+          name="lastName"
+          onChange={handleChange}
+          value={values.lastName}
+        />
         <span>Proporciona un apellido valido</span>
         <br />
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" />
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={values.email}
+        />
         <span>Proporciona un email valido</span>
         <br />
         <button type="submit">submit</button>
